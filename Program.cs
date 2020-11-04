952055129ab4b2acb04971501f7578901cba6117
oskar-t-fredriksson <72459215+oskar-t-fredriksson@users.noreply.github.com>
2020-11-04 11:42:08 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectTest
+{
+    class Program
+    {
+        class Person 
+        {
+            public string name, phone;
+        }
+        static void Main(string[] args)
+        {
+            //Variabellösning
+            string name, phone;
+            name = "Arne Anka"; phone = "013-13 13 13";
+            Console.WriteLine("{0}, tel. {1}", name, phone);
+            //Objektlösning
+            Person P = new Person();
+            P.name = "Arne Anka"; P.phone = "013-13 13 13";
+            Console.WriteLine("{0}, tel. {1}", P.name, P.phone);
+        }
+    }
+}
